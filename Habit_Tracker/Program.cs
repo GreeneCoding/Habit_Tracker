ebb0fffb2f58cf30999661ae2890a96193596f23
Daniel Greene <dgreene@support.proed.org>
2022-06-16 16:40:34 -0400

    Added UserInput method to show console user what the application can do. Also, added GetAllRecords method to create a way to grab all records in the reading table.

--- Habit_Tracker/Program.cs
+++ Habit_Tracker/Program.cs
@@ -15,8 +15,48 @@ void CreateDatabase()
             tableCmd.ExecuteNonQuery();
         }
     }
-    void GetUserInput();
-    { 
-    
+}
+
+void GetAllRecords()
+{
+    using (var connection = new SqliteConnection(connectionString))
+    {
+        using (var tableCmd = connection.CreateCommand())
+        {
+            connection.Open();
+            tableCmd.CommandText = @"CREATE TABLE IF NOT EXISTS Reading (Id INTEGER PRIMARY KEY AUTOINCREMENT, Date TEXT, Quantity INTEGER)";
+            tableCmd.ExecuteNonQuery();
+        }
+    }
+}
+
+GetUserInput();
+
+void GetUserInput()
+{
+    Console.Clear();
+    bool closeApp = false;
+    while (closeApp == false)
+    {
+        Console.WriteLine("\nWelcome to the Habit Tracker App");
+        Console.WriteLine("\nHow would you like to proceed?");
+        Console.WriteLine("\nType 0 to exit the application");
+        Console.WriteLine("Type 1 to view all records");
+        Console.WriteLine("Type 2 to create a new entry");
+        Console.WriteLine("Type 3 to delete an entry");
+        Console.WriteLine("Type 4 to update an entry");
+
+        string commandInput = Console.ReadLine();
+
+        switch (commandInput)
+        {
+            case "0":
+                Console.WriteLine("\nGoodBye!\n");
+                closeApp = true;
+                break;
+            case "1":
+                GetAllRecords();
+                break;
+        }
     }
 }
