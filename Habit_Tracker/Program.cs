155766ac383294733622373228bbe56f05a4fdb9
Daniel Greene <dgreene@support.proed.org>
2022-06-16 13:26:49 -0400

    Commiting the Check for a Sqlite database and Create Database method.

--- Habit_Tracker/Program.cs
+++ Habit_Tracker/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.Sqlite;
+
+string connectionString = @"Data Source=habit_tracker.db";
+
+CreateDatabase();
+
+void CreateDatabase()
+{
+    using (var connection = new SqliteConnection(connectionString))
+    {
+        using (var tableCmd = connection.CreateCommand())
+        {
+            connection.Open();
+            tableCmd.CommandText = @"CREATE TABLE IF NOT EXISTS Reading (Id INTEGER PRIMARY KEY AUTOINCREMENT, Date TEXT, Quantity INTEGER)";
+            tableCmd.ExecuteNonQuery();
+        }
+    }
+    void GetUserInput();
+    { 
+    
+    }
+}
